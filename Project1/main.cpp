4af2f73b15d3c9e80128dd985c475c42594bc319
Nathan Burlis <43301944+burlisn@users.noreply.github.com>
2020-04-13 21:15:13 -0500

    Initial setup

---
+++ Project1/main.cpp
@@ -0,0 +1,14 @@
+// Programmers: Nathan Burlis & Carter Scranton
+// Class:		CpE 3110
+// Assignment:  Project 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int iterNum; // number of iterations
+	int numArith; // number of additions/subtractions
+
+	return 0;
+}
