281cc46a883976fb6443d553479f04caa26bdc8e
Nathan Burlis <43301944+burlisn@users.noreply.github.com>
2020-04-16 09:25:41 -0500

    works with all possible values

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -12,31 +12,163 @@ using namespace std;
 
 int main()
 {
-	int iterNum; // number of iterations
-	int numArith; // number of additions/subtractions
+	int iterNum=0; // number of iterations
+	int numArith=0; // number of additions/subtractions
 
 	ifstream file{ "data.txt" };
 	string testinput;
 
-	bitset <12> mc, mp, r1, r2; //mc=multiplicand, mp=multiplier, r1=more significant resulst, r2=less significant result
+	bitset <4> mc4, mp4, r1_4, r2_4; //mc=multiplicand, mp=multiplier, r1=more significant resulst, r2=less significant result
+	bitset <5> mc5, mp5, r1_5, r2_5;
+	bitset <6> mc6, mp6, r1_6, r2_6;
+	bitset <7> mc7, mp7, r1_7, r2_7;
+	bitset <8> mc8, mp8, r1_8, r2_8;
+	bitset <9> mc9, mp9, r1_9, r2_9;
+	bitset <10> mc10, mp10, r1_10, r2_10;
+	bitset <11> mc11, mp11, r1_11, r2_11;
+	bitset <12> mc12, mp12, r1_12, r2_12; 
 	bitset <1> c_out = 0; //c_out used in add function & shift function
 
 	file >> testinput; //DO NOT DELTE, "PART" OF WHILE LOOP TO MAKE SURE READING IS CORRECT!!!
 	while (file)
 	{
-		string_to_bits(mc, testinput);
-		cout << mc << " ";
+		switch (testinput.length())
+		{
+		case 4:
+			string_to_bits(mc4, testinput);
+			cout << mc4 << " ";
 
-		file >> testinput;
-		string_to_bits(mp, testinput);
-		cout << mp << endl;
+			file >> testinput;
+			string_to_bits(mp4, testinput);
+			cout << mp4 << endl;
 
-		add_and_shift(r1, r2, mc, mp, c_out);
-		cout << "Add and shift results: \n"<< r1 << r2 << endl;
+			add_and_shift(r1_4, r2_4, mc4, mp4, c_out);
+			cout << "Add and shift results: \n" << r1_4 << r2_4 << endl;
 
-		booths(r1, r2, mc, mp);
-		cout << "Booth's algorithm results: \n" << r1 << r2 << endl << endl;
+			booths(r1_4, r2_4, mc4, mp4);
+			cout << "Booth's algorithm results: \n" << r1_4 << r2_4 << endl << endl;
+			break;
 
+		case 5:
+			string_to_bits(mc5, testinput);
+			cout << mc5 << " ";
+
+			file >> testinput;
+			string_to_bits(mp5, testinput);
+			cout << mp5 << endl;
+
+			add_and_shift(r1_5, r2_5, mc5, mp5, c_out);
+			cout << "Add and shift results: \n" << r1_5 << r2_5 << endl;
+
+			booths(r1_5, r2_5, mc5, mp5);
+			cout << "Booth's algorithm results: \n" << r1_5 << r2_5 << endl << endl;
+			break;
+
+		case 6:
+			string_to_bits(mc6, testinput);
+			cout << mc6 << " ";
+
+			file >> testinput;
+			string_to_bits(mp6, testinput);
+			cout << mp6 << endl;
+
+			add_and_shift(r1_6, r2_6, mc6, mp6, c_out);
+			cout << "Add and shift results: \n" << r1_6 << r2_6 << endl;
+
+			booths(r1_6, r2_6, mc6, mp6);
+			cout << "Booth's algorithm results: \n" << r1_6 << r2_6 << endl << endl;
+			break;
+
+		case 7:
+			string_to_bits(mc7, testinput);
+			cout << mc7 << " ";
+
+			file >> testinput;
+			string_to_bits(mp7, testinput);
+			cout << mp7 << endl;
+
+			add_and_shift(r1_7, r2_7, mc7, mp7, c_out);
+			cout << "Add and shift results: \n" << r1_7 << r2_7 << endl;
+
+			booths(r1_7, r2_7, mc7, mp7);
+			cout << "Booth's algorithm results: \n" << r1_7 << r2_7 << endl << endl;
+			break;
+
+		case 8:
+			string_to_bits(mc8, testinput);
+			cout << mc8 << " ";
+
+			file >> testinput;
+			string_to_bits(mp8, testinput);
+			cout << mp8 << endl;
+
+			add_and_shift(r1_8, r2_8, mc8, mp8, c_out);
+			cout << "Add and shift results: \n" << r1_8 << r2_8 << endl;
+
+			booths(r1_8, r2_8, mc8, mp8);
+			cout << "Booth's algorithm results: \n" << r1_8 << r2_8 << endl << endl;
+			break;
+
+		case 9:
+			string_to_bits(mc9, testinput);
+			cout << mc9 << " ";
+
+			file >> testinput;
+			string_to_bits(mp9, testinput);
+			cout << mp9 << endl;
+
+			add_and_shift(r1_9, r2_9, mc9, mp9, c_out);
+			cout << "Add and shift results: \n" << r1_9 << r2_9 << endl;
+
+			booths(r1_9, r2_9, mc9, mp9);
+			cout << "Booth's algorithm results: \n" << r1_9 << r2_9 << endl << endl;
+			break;
+
+		case 10:
+			string_to_bits(mc10, testinput);
+			cout << mc10 << " ";
+
+			file >> testinput;
+			string_to_bits(mp10, testinput);
+			cout << mp10 << endl;
+
+			add_and_shift(r1_10, r2_10, mc10, mp10, c_out);
+			cout << "Add and shift results: \n" << r1_10 << r2_10 << endl;
+
+			booths(r1_10, r2_10, mc10, mp10);
+			cout << "Booth's algorithm results: \n" << r1_10 << r2_10 << endl << endl;
+			break;
+
+		case 11:
+			string_to_bits(mc11, testinput);
+			cout << mc11 << " ";
+
+			file >> testinput;
+			string_to_bits(mp11, testinput);
+			cout << mp11 << endl;
+
+			add_and_shift(r1_11, r2_11, mc11, mp11, c_out);
+			cout << "Add and shift results: \n" << r1_11 << r2_11 << endl;
+
+			booths(r1_11, r2_11, mc11, mp11);
+			cout << "Booth's algorithm results: \n" << r1_11 << r2_11 << endl << endl;
+			break;
+
+		case 12:
+			string_to_bits(mc12, testinput);
+			cout << mc12 << " ";
+
+			file >> testinput;
+			string_to_bits(mp12, testinput);
+			cout << mp12 << endl;
+
+			add_and_shift(r1_12, r2_12, mc12, mp12, c_out);
+			cout << "Add and shift results: \n" << r1_12 << r2_12 << endl;
+
+			booths(r1_12, r2_12, mc12, mp12);
+			cout << "Booth's algorithm results: \n" << r1_12 << r2_12 << endl << endl;
+			break;
+		}
 		file >> testinput;
 	}
 
